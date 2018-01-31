835eb1bab1c096185c542d9421ac1ff13b879548
Juan Ferreira <juan.ferreira@youvisit.com>
2018-01-31 12:33:00 -0500

    Add apply middleware support

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
 const express = require('express')
+const cors = require('cors')
 const graphqlHTTP = require('express-graphql')
 
-module.exports = (options) => {
+module.exports = (options, applyMiddleware) => {
 
   let app = express()
+
+  if ( typeof applyMiddleware == 'function' )
+    applyMiddleware(app);
 
   app.use(graphqlHTTP(options))
 
